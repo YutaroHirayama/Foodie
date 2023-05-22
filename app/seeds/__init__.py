8e808a6ed4c4b0b854f8bf398c0e4b1f185d0316
Yutaro <yutarohirayama@gmail.com>
2023-05-22 09:20:45 -0700

    coded out undo reviewImages

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,7 +20,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_reviewImages()
+        # undo_reviewImages()
         undo_businessImages()
         undo_reviews()
         undo_businesses()
@@ -34,7 +34,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_reviewImages()
+    # undo_reviewImages()
     undo_businessImages()
     undo_reviews()
     undo_businesses()
