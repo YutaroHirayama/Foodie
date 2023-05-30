b3f300d3f629db90fb64e0a21ec8ca3c9a2b2f1b
Yutaro <yutarohirayama@gmail.com>
2023-05-30 08:12:18 -0700

    final

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, IntegerField, StringField
-from wtforms.validators import DataRequired, ValidationError, Length, NumberRange, Optional
+from wtforms.validators import DataRequired, ValidationError, Length, NumberRange, Optional, URL
 
 def reviewImage_check(form, field):
     reviewImage = field.data
@@ -13,6 +13,6 @@ class ReviewForm(FlaskForm):
 
     reviewText = TextAreaField('reviewText', validators=[DataRequired(), Length(max=2000, message='Review can not exceed 2,000 characters.')])
     rating = IntegerField('rating', validators=[DataRequired(message='You must select a rating.'), NumberRange(min=1, max=5, message='Rating must be between 1 and 5.')])
-    image1 = StringField('image1', validators=[Optional(strip_whitespace=True), reviewImage_check])
-    image2 = StringField('image2', validators=[Optional(strip_whitespace=True), reviewImage_check])
-    image3 = StringField('image3', validators=[Optional(strip_whitespace=True), reviewImage_check])
+    image1 = StringField('image1', validators=[Optional(strip_whitespace=True), URL(message='Image must be a valid URL.'), reviewImage_check])
+    image2 = StringField('image2', validators=[Optional(strip_whitespace=True), URL(message='Image must be a valid URL.'), reviewImage_check])
+    image3 = StringField('image3', validators=[Optional(strip_whitespace=True), URL(message='Image must be a valid URL.'), reviewImage_check])
