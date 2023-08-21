bd2a625eef850f6d9c55e12dfcef22e091aefdbe
Yutaro <yutarohirayama@gmail.com>
2023-08-20 20:56:13 -0700

    toggle updates

--- react-app/src/components/ProfilePage/manageToggle.js
+++ react-app/src/components/ProfilePage/manageToggle.js
@@ -1,9 +1,26 @@
 
 
-const togglePage = ({business, idx, type}) => {
+const togglePage = ({type}) => {
 
 
   return (
-    <></>
+    <div className='profile-page-businesses'>
+      <h1 className='profile-page-businesses-owned-header'>Manage your Businesses</h1>
+      <div className='profile-page-create-business'>
+        <OpenModalButton
+          buttonText='Create a Business Page'
+          className='foodie-big-button'
+          icon="fa-regular fa-building"
+          modalComponent={<CreateBusinessModal user={user} />}
+          />
+      </div>
+      <div className='profile-page-businesses-owned'>
+        {businessesOwned && businessesOwned.map((business, idx) => (
+          <div className='owned-business-card'>
+            <BusinessCard key={idx} business={business} idx={idx+1} type='private'/>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
